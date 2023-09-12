0ae6aaaf908066734fdc5edbea942822a62f03d4
Jayant Kumar Jha <144110013+jayantmuku@users.noreply.github.com>
2023-09-12 13:46:37 +0530

    Add files via upload

---
+++ upper&lower string.py
@@ -0,0 +1,22 @@
+name=input("Enter your first name:")
+print(name.upper())
+print(name.lower())
+
+
+
+
+
+word=input("Enter any string:")
+print(word.title())
+print(word.count("e"))
+
+
+
+
+
+word=input("Enter any string:")
+alpha=input("Enter alpha to count")
+print("occurance of ",alpha,"is=",word.count(alpha),"times")
+
+
+
